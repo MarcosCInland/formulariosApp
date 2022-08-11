64693f5df690db7b7a3af44f3c1abda006a508ff
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-08-11 15:48:47 -0600

    template - dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre    : string;
+  favoritos : Favorito[];
+}
+
+interface Favorito {
+  id    : number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,9 +18,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  persona: Persona = {
+    nombre: 'Marcos',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'Death Stranding'}
+    ]
+  }
+
+  nuevoJuego: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(): void {
+    console.log('xx')
+  }
+
+  eliminar(i: number) : void {
+    this.persona.favoritos.splice(i, 1);
+  }
+
+  agregar() : void {
+    const nuevoFav: Favorito = { id: this.persona.favoritos.length+1, nombre: this.nuevoJuego };
+    this.persona.favoritos.push({...nuevoFav});
+    this.nuevoJuego = '';
   }
 
 }
