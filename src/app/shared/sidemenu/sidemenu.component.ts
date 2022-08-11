f81142c3df107e65307e4902e4013b86bcd755dd
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-08-11 11:23:31 -0600

    template basicos form creado

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem{
+  texto : string;
+  ruta  : string;
+}
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
@@ -8,6 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidemenuComponent implements OnInit {
 
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    },
+  ]
+
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miForm!: NgForm  //Hacer referencia a un objeto local del .html
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  productoInvalido(): boolean {
+    return this.miForm?.controls.producto?.invalid && this.miForm?.controls.producto?.touched;
+  }
+
+  precioInvalido(): boolean {
+    return this.miForm?.controls.precio?.value < 0 && this.miForm?.controls.precio?.touched;
+  }
+
+  guardar() {
+    console.log(this.miForm);
   }
 
 }
