9ab331f50c609f0243f72766a851ba69ac678dff
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-08-12 11:33:11 -0600

    template: checks, switch & radios

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -8,8 +8,9 @@ interface MenuItem{
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [`
+    li { cursor: pointer}
+  `]
 })
 export class SidemenuComponent implements OnInit {
 
