1f692b0d3ae066604b5b680bbca6bccae171ee1d
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-08-19 16:47:20 -0600

    formularios basicos y dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  campoValido(campo: string) {
+    return this.miForm.controls[campo].errors && this.miForm.controls[campo].touched;
+  }
+
+  guardar(): void {
+    if (this.miForm.invalid) {
+      this.miForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.miForm.value);
+    //this.miForm.reset();
   }
 
 }
